5dfe1804d035929e6ccd2623bb2e45ecca4c704e
Jxse0 <jm.expositojavier@gmail.com>
2024-03-01 00:15:49 +0100

    init Next.js

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          HI
+        </div>
+      </main>
+    </React.Fragment>
+  );
+}
